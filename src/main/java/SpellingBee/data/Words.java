bbfe732a0f944ac6e30206511692c0603c53fa83
jchernin4 <jeremychernin@gmail.com>
2021-06-14 00:09:44 -0400

    More words

--- src/main/java/SpellingBee/data/Words.java
+++ src/main/java/SpellingBee/data/Words.java
@@ -2,6 +2,28 @@ package SpellingBee.data;
 
 public class Words {
     public static String[] words = new String[]{
-            "test"
+            "acuate",
+            "frequency",
+            "astonishing",
+            "allocation",
+            "memorandum",
+            "deprivation",
+            "orientation",
+            "laboratory",
+            "conscience",
+            "constellation",
+            "hypothesize",
+            "coup",
+            "doughty",
+            "inveigh",
+            "obdurate",
+            "silhouette",
+            "transmogrify",
+            "autochthonous",
+            "misspell",
+            "handkerchief",
+            "boulevard",
+            "presbyterian",
+            "resplendent"
     };
 }
